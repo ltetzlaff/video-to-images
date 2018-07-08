c0fb14dc3a0dff0522389574c254623629f64748
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-07-08 22:25:51 +0200

    :hammer: remove duplicate code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import { exec } from "improved/dist/process"
-import { path as ffmpegPath } from "ffmpeg-static"
 import { createIfDoesntExist, getFolderName } from "improved/dist/fs"
+import ffmpeg from "./ffmpeg"
 
 /**
  * @param outputPathPattern e.g. $img_%04d.png
@@ -14,7 +13,7 @@ export default async function(
   await createIfDoesntExist(getFolderName(outputPathPattern))
 
   const inputArgs = ["-i", videoFilePath]
-  const fpsArgs = fps === undefined ? [] : ["-r", fps]
+  const fpsArgs = fps === undefined ? [] : ["-r", fps.toString()]
   const args = [...inputArgs, ...fpsArgs, ...customArgs, outputPathPattern]
-  return exec(`${ffmpegPath} ${args.join(" ")}`)
+  return ffmpeg(args)
 }
